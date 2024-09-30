68f815befffbac1d5523d2fb3f9cfc439419b5b4
Mihai Ilie <mihai.ilie@nxdata.ro>
2024-09-30 21:32:52 +0300

    variabile

---
+++ curs_01_data_structures/strings.py
@@ -0,0 +1,63 @@
+""" siruri de caractere, imutabile si indexabile"""
+
+# "programare"
+
+var = str()
+
+""" concatenare """
+
+var_1 = 'pro'
+var_2 = 'gram'
+var_3 = 'are'
+
+# print(var_1 + var_2 + var_3)
+
+# 'aaaa'
+# "aaaa"
+# x = """Maria's toy"""
+# print(x)
+
+""" multiplicare """
+
+# multip = 4
+# print(multip * var_1, 'a')
+# print(id(-4 * var_1))
+# print(id(""))
+
+""" transformare in string str()"""
+# print(type(str(False)))
+
+""" lungimea unui string len() """
+# print(len(var_1))
+
+""" operatori in si not in """
+# print('gr' in var_2)
+# print('gr' not in var_2)
+
+""" indexare """ # var[index]
+var_index = 'programare'
+            #0123456789
+# print(var_index[4])
+# print(var_index[9])
+# # print(var_index[10])
+# print(var_index[-1])
+# print(var_index[len(var_index)-1])
+# print(var_index[-len(var_index)])
+
+""" slicing """ # [start:stop:pasul]
+# print(var_index[3:10:4])
+# print(var_index[:5])
+# print(var_index[::-1])
+
+""" cateva metode folosite ptr obiectele de tip string """
+# capitalize() - > primul char cu litera mare
+# upper() - > toate char cu litera mare
+# lower() - > toate char cu litera mica
+# count(parametru) -> numara de cate ori gaseste parametru in string
+# var_capital = var_index.capitalize()
+# print(var_capital)
+
+
+
+""" interpolare la stringuri """
+# TODO: de scris interpolarea
---
+++ curs_01_data_structures/variabile.py
@@ -0,0 +1,61 @@
+# var = '101'
+# var2 = 101
+# # id(var)
+# print(id(var))
+# print(id(var2))
+
+# venitul saptamanal al unui salariat
+
+# weekly_income =
+# nr_of_hours = 40
+# hourly_income = 10
+# weekly_income = nr_of_hours * hourly_income
+# print(weekly_income)
+
+# nrofhours   asa nu
+# NOH asa nu
+# 1_aaa =  asa nu
+# nr of hours = asa nu
+# def, class, pass, import, del, and, in, is, if, else  asa nu
+
+# first_number = -1 # integer
+# # first_Number = first_number + 1
+# # print(first_number)
+#
+# # type()
+# print(type(first_number))
+#
+# second_number = 1.1 # float
+# print(type(second_number))
+#
+# third_number = 1j # complex
+# print(type(third_number))
+
+# print(type(str(first_number)))
+# operatii matematice +, -, *, /, **, %, //
+
+
+# var_3 = 'bsamzvhsdvakkcaslkh' # string
+# print(type(var_3))
+
+
+# var_4 = True # (False)
+# print(type(var_4))
+# var_4_false = False # 0, '', [], {}, (), None
+#
+# var_5 = None
+
+# operatori de comparare ==, !=, >, <, >=, <=
+# operatori logici and, or, not
+# operatori identitate is, is not
+# operatori apartenenta in, not in
+
+# var_str = 'George'
+# print('g' in var_str)
+
+# test = input("Introduceti un nr: ")
+# test_2 = len(test)
+# print(test_2)
+
+# test_3 = 10
+# print(len(test_3))
