c05640c8bb2f8db85aafd4d4c4599983bf7c8b82
Mihai Ilie <mihai.ilie@nxdata.ro>
2024-12-09 21:34:48 +0200

    django

---
+++ django_project/proiect/aplicatie1/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Location(models.Model): # aplicatie1_location
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=90)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.country} -> {self.city}'
---
+++ django_project/proiect/aplicatie1/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+
+# CreateView
+# DetailView
+# ListView
+# -------
+
+class LocationView(ListView):
+    pass
